ae018634992b50e6257815198c62f40cfae49256
sophiaestevam <sophiaestevamferreira@gmail.com>
2025-10-02 15:35:11 -0300

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,12 @@
+const computador = {
+    marca: "Positivo",
+    processador: "core 13",
+    memoria: "8GB",
+    hd: = "1TB"
+}
+
+function criarNo(){
+    const elementoMarca = document.createElement(h1)
+    elementoMarca.innerText = `${computador.marca}`
+    const elementoProc = document.createElement(h2)
+    elementoProc.innerText = `${computador.processador}`
